966221c92cc68bcf894a79740c32784051ec85f9
Josphin <josphinjerald8@gmail.com>
2024-05-25 14:28:09 +0530

    updating

--- demo.py
+++ demo.py
@@ -5,8 +5,8 @@ if n < 0:
     print("Please enter a non-negative integer.")
 else:
   
-    factorial = 1
-    for i in range(1, n + 1):
+    factorial = 2
+    for i in range(2, n + 1):
         factorial *= i
     
   
